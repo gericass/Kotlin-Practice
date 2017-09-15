2b8f19e128579696aeac2e1251a900041df52e27
Keita <bonjovi1926@yahoo.co.jp>
2017-09-16 01:56:56 +0900

    add: generics

--- src/generics.kt
+++ src/generics.kt
@@ -1,6 +1,12 @@
 class gen<T>(var value: T)
 
+fun <T> genfunc(value: T): String {
+    return value.toString()
+}
+
 fun main(args: Array<String>){
     val intContainer = gen<Int>(123)
     println(intContainer.value)
+
+    println(genfunc("hahaha"))
 }
