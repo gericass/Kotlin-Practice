ef56d4dae5abc58e2a8f84f66c79fcd310cdb558
Keita <bonjovi1926@yahoo.co.jp>
2017-09-15 20:20:48 +0900

    add: constructor

--- src/inheritance.kt
+++ src/inheritance.kt
@@ -1,12 +1,24 @@
 
 
+interface test {
+    fun printNumber(num: Int)
+}
+
 open class Person(val name: String){
-    fun introduceMyself(){
+    open fun introduceMyself(){
         println("I am $name")
     }
 }
 
-class Student(name: String, val id: Long): Person(name)
+class Student(name: String, val id: Long): Person(name),test { //クラスとインターフェースは同時に継承できる（使うのかは知らん）
+    override fun introduceMyself() {
+        super.introduceMyself() //スーパークラスでの実装も使える
+        println("I am $name(id=$id)")
+    }
+    override fun printNumber(num: Int){
+        println(num+10)
+    }
+}
 
 fun main(args: Array<String>){
     val person: Person = Person("ゆたか")
@@ -16,5 +28,6 @@ fun main(args: Array<String>){
     println(student.id)
     println(student.name)
     student.introduceMyself()
+    println(student.printNumber(15))
 
 }
