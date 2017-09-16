4c1913c99aae10f62bc9a3ee09b8a804cd9e33f6
Keita <bonjovi1926@yahoo.co.jp>
2017-09-16 17:14:06 +0900

    add: null

--- src/null.kt
+++ src/null.kt
@@ -1,5 +1,41 @@
 
-fun main(args: Array<String>){
-    var s: String? = null //型名の後に?をつけるとnullが代入可
-    println(s)
+
+fun checkNull() {
+    var foo: String? = "Not Null"
+    println(foo)
+    foo = null
+    println((foo ?: "nullだった").toUpperCase()) //fooがnullだった場合fooの代わりに"nullだった"に対してtoUpperCase()が実行される（エルビス演算子）
+
+
 }
+
+
+class myString(var str: String = "Hello "){
+    fun String.addStr() { //同一クラス内からしか呼べない
+        str += this
+    }
+
+    fun somes(value: String) {
+        value.addStr()
+    }
+}
+
+fun main(args: Array<String>) {
+    val s: String? = "Hello World" //型名の後に?をつけるとnullが代入可
+    println(s)
+    if (s != null) { // nullを判定することで元の型のメソッドが使える
+        println(s.hashCode())
+    } else {
+        println("null value")
+    }
+
+
+    val z = myString()
+    z.somes("World!")
+    println(z.str)
+
+    checkNull()
+
+
+}
+
